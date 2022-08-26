ba4f6fff25c96f14bc6eebc2aeedb7edd7c81f4a
Joan Skënderi <jskenderi@outlook.com>
2022-08-26 17:05:16 +0200

    Friday Commit

--- screens/CategoriesScreen.jsx
+++ screens/CategoriesScreen.jsx
@@ -3,19 +3,21 @@ import CategoryGridTile from "../components/CategoryGridTile";
 
 import { CATEGORIES } from "../data/dummy-data";
 
-function pressHandler() {}
+function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview");
+    }
 
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile
-      title={itemData.item.title}
-      color={itemData.item.color}
-      onPress={pressHandler}
-    />
-  );
-}
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
 
-function CategoriesScreen() {
   return (
     <FlatList
       data={CATEGORIES}
